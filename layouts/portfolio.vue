21d503fc732be08924818b2547e8a94f1ef5739b
Leon Si <contact@leonzalion.com>
2022-04-20 20:50:05 -0400

    fix formatting

--- layouts/portfolio.vue
+++ layouts/portfolio.vue
@@ -1,10 +1,9 @@
 <template>
 	<div class="slidev-layout">
-		<div class="column justify-stretch h-full">
+		<div id="content" class="column justify-stretch h-full">
 			<div class="h-2 bg-black mb-4"></div>
-			<slot id="content"></slot>
-			<div class="flex-1"></div>
-			<div class="h-2 bg-black mt-4"></div>
+			<slot></slot>
+			<div class="h-2 bg-black mt-auto"></div>
 		</div>
 	</div>
 </template>
